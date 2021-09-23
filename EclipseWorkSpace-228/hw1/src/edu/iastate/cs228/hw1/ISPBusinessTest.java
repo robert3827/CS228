a01a182abf755fd20f894c97b4afe44ccd5694e3
robert3827 <32315287+robert3827@users.noreply.github.com>
2021-09-22 20:59:40 -0500

    all done?

--- EclipseWorkSpace-228/hw1/src/edu/iastate/cs228/hw1/ISPBusinessTest.java
+++ EclipseWorkSpace-228/hw1/src/edu/iastate/cs228/hw1/ISPBusinessTest.java
@@ -2,13 +2,51 @@ package edu.iastate.cs228.hw1;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.io.FileNotFoundException;
+
 import org.junit.jupiter.api.Test;
 
 class ISPBusinessTest {
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void testUpdatePlain() {
+		try {	
+			Town town = new Town("ISP4x4.txt");
+			ISPBusiness isp = new ISPBusiness();
+			String s;
+			
+			Town newT = isp.updatePlain(town);
+			for(int x=0;x<town.getLength();x++) {
+				for(int y=0;y<town.getWidth();y++) {
+					town.grid[x][y] = newT.grid[x][y];
+					
+				}
+			}
+			s = "E E E E \n"
+					+ "C C O E \n"
+					+ "C O E O \n"
+					+ "C E E E \n";	
+			assertEquals(s, newT.toString());
+		} catch (Exception e) {
+			System.out.println("That File Doesn't Exist");
+			e.printStackTrace();
+		}
 	}
+	@Test
+	void testgetProfit() {
+		try {
+			Town town = new Town("ISP4x4.txt");
+			ISPBusiness isp = new ISPBusiness();
+			int d = isp.getProfit(town);
+			
+			assertEquals(1, d);
+			
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	
+	}
+	
 
 }
