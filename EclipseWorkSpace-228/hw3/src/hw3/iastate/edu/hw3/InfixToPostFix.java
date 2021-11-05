b7f187c1c8537ad990087f48f9dffbd3b3a5b62f
robert3827 <32315287+robert3827@users.noreply.github.com>
2021-11-05 12:35:55 -0500

    going to handle all of the exceptions with in the converter

--- EclipseWorkSpace-228/hw3/src/hw3/iastate/edu/hw3/InfixToPostFix.java
+++ EclipseWorkSpace-228/hw3/src/hw3/iastate/edu/hw3/InfixToPostFix.java
@@ -5,21 +5,34 @@ import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Scanner;
 import java.util.Stack;
-
+/**
+ * Takes a file from input. Reads the file in infix notation and converts it to postfix notation. 
+ * Outputs either the postfix notation if the infix expression is valid, otherwise the first error encountered.
+ * @author Robert Holeman
+ *
+ */
 public class InfixToPostFix {
-	private int curRank = 0;
+	/**
+	 * Handles getting the input and running the code.
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		InfixToPostFix ipf = new InfixToPostFix();
 		Scanner scan = new Scanner(System.in);
 		System.out.println("Input File Name");
 		String fileName = scan.next();
-		ipf.inputFile(fileName);
+		ArrayList<String> exprs = ipf.inputFile(fileName);
 		
+			ipf.convertInfixToPostfix(exprs);		
 		
 		scan.close();
 	}
 
-	
+	/**
+	 * Reads from a file. Each line is taken as one expression and added to the list of expressions, which are stored as strings.
+	 * @param s the name of the file to read from
+	 * @return A list of strings representing individual expressions in infix notation
+	 */
 	private ArrayList<String> inputFile(String s) {
 		ArrayList<String> exprList = new ArrayList<>();
 		File file = new File(s);
@@ -38,61 +51,81 @@ public class InfixToPostFix {
 			
 		}
 		
-		
-		
-		
-		
 		return exprList;
 	}
-
-	private ArrayList<String> convertInfixToPostfix(ArrayList<String> ifList) throws Exception {
+	/**
+	 * 
+	 * @param ifList list of infix expressions to be converted to postfix
+	 * @return list of strings representing postfix expressions or error messages
+	 */
+	private ArrayList<String> convertInfixToPostfix(ArrayList<String> ifList){
 		ArrayList<String> pfList = new ArrayList<>();
 		for(String s: ifList) {
-			convert(s);
+			try {
+				convert(s);
+			} catch (Exception e) {
+			
+				e.printStackTrace();
+			}
 		}
 	
 	
 	
 		return pfList;
 	}
+	/**
+	 * 
+	 * @param infix a string representing one infix notation 
+	 * @return the correct postfix expression from the given infix expression
+	 * @throws Exception Throws exception to stop program execution. Could also just return a string there but TBD on what is better.
+	 */
 	private String convert(String infix) throws Exception {
-		infix.trim();
+		int curRank = 0;
+		Scanner scan = new Scanner(infix);
 		String postFix = "";
-		Stack<Character> stack = new Stack<>();
-		for(int i=0;i<infix.length();i++) {
-			char c = infix.charAt(i);
-			curRank += rank(c);//as soon as I see the char I want to make sure the rank is ok.
-			if(curRank > 1 ) {
-				throw new Exception("Too Many Operands" + " (" + c + ")");
-			} else if(curRank<-1) {
-				throw new Exception("Too Many Operators" + " (" + c + ")");
-			}
-			if(Character.isLetterOrDigit(c)) {
-				postFix += c;
-			} else {//c is an operator
-				;
-				int ropOfStackPrec = precedenceInStack(stack.peek());
-				int cPrec;
+		Stack<String> stack = new Stack<>();
+		while( scan.hasNext()) {
+			
+			
+				String s = scan.next();
 				
+				curRank += rank(s);//as soon as I see the char I want to make sure the rank is ok.
+				if(curRank > 1 ) {
+					throw new Exception("Too Many Operands" + " (" + s + ")");
+				} else if(curRank<-1) {
+					throw new Exception("Too Many Operators" + " (" + s + ")");
+				}
+				//TODO Here is the real logic.
+				if(rank(s)==1) {//not operator or parentheses
+					postFix += s;
+				} else {//c is an operator
+					int stackOpPrec = precedenceInStack(stack.peek());
+					int curOpPrec = precedenceAsInput(s);
+					if(stackOpPrec>=curOpPrec) {
+						postFix+=stack.pop();
+					}
+					
+					
+				}
 			}
-		}
+			
 		
 		
 		
 		return postFix;
 	}
-	private int rank(char c) {
+	private int rank(String s) {
 		int rank;
-		switch (c) {
-		case '+': rank = -1;break;
-		case '-': rank = -1;break;
-		case '*': rank = -1;break;
-		case '/': rank = -1;break;
-		case '%': rank = -1;break;
-		case '^': rank = -1;break;//Is this Correct
-		//All Parentheses are Equivalent
-		case '(': rank = 0;break;
-		case ')': rank = 0;break;
+		switch (s) {
+		case "+": rank = -1;break;
+		case "-": rank = -1;break;
+		case "*": rank = -1;break;
+		case "/": rank = -1;break;
+		case "%": rank = -1;break;
+		case "^": rank = -1;break;//Is this Correct
+		
+		case "(": rank = 0;break;
+		case ")": rank = 0;break;
 		
 		
 		default:
@@ -101,45 +134,44 @@ public class InfixToPostFix {
 		return rank;
 	}
 	
-	private int precedenceAsInput(char c) {
+	private int precedenceAsInput(String s) {
 		int prec;
-		switch (c) {
+		switch (s) {
+		case "+": prec = 1;break;
+		case "-": prec = 1;break;
 		
-		case '+': prec = 1;break;
-		case '-': prec = 1;break;
+		case "*": prec = 2;break;
+		case "/": prec = 2;break;
+		case "%": prec = 2;break;
 		
-		case '*': prec = 2;break;
-		case '/': prec = 2;break;
-		case '%': prec = 2;break;
-		
-		case '^': prec = 4;break;//Is this Correct
-		//All Parentheses are Equivalent
-		case '(': prec = 5;break;
-		case ')': prec = 0;break;
+		case "^": prec = 4;break;//Is this Correct
+
+		case "(": prec = 5;break;
+		case ")": prec = 0;break;
 		
 		default:
-			throw new IllegalArgumentException("Invalid Operator: " + c);
+			throw new IllegalArgumentException("Invalid Operator: " + s);
 		}
 		return prec;
 	}
-	private int precedenceInStack(char c) {
+	private int precedenceInStack(String s) {
 		int prec;
-		switch (c) {
-		case '+': prec = 1;break;
-		case '-': prec = 1;break;
+		switch (s) {
+		case "+": prec = 1;break;
+		case "-": prec = 1;break;
 		
-		case '*': prec = 2;break;
-		case '/': prec = 2;break;
-		case '%': prec = 2;break;
+		case "*": prec = 2;break;
+		case "/": prec = 2;break;
+		case "%": prec = 2;break;
 		
-		case '^': prec = 3;break;//Is this Correct
-		//All Parentheses are Equivalent
-		case '(': prec = -1;break;
-		case ')': prec = -0;break;
+		case "^": prec = 3;break;//Is this Correct
+		
+		case "(": prec = -1;break;
+		case ")": prec = -0;break;
 		
 		
 		default:
-			throw new IllegalArgumentException("Invalid Operator: " + c);
+			throw new IllegalArgumentException("Invalid Operator: " + s);
 		}
 		return prec;
 	}
